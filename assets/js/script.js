7e032be7544618f6ee2df8d240604389ac53691b
Jessica <sevorg.jess@gmail.com>
2021-11-28 22:23:11 -0700

    formatting to time slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,76 @@
 var dateToday = document.querySelector('#currentDay');
 var container = document.querySelector('.container')
 
+var workHours = ['8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '11:00pm']
 
 var today = moment(). format("dddd, MMMM Do, YYYY");
-console.log(today);
+var timeNow = moment().format('LT', 'h:mma');
+// console.log(today);
+// console.log(timeNow)
 
-var workHours = ['8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM']
 
 
-
+// fill current date in top portion
 dateToday.textContent = today;
 
+// set up page with 8 hour work day increments
+
 var buildTimes = function() {
-
-for (let i = 0; i < workHours.length; i++) {
-  var row = document.createElement('div');
-  row.className = 'row'
-  container.appendChild(row)
-
-  var smallCard = document.createElement('div')
-  smallCard.className = 'col-2 card';
-  // smallCard.textContent = workHours[i];
-  row.appendChild(smallCard)
-
-  var timeSlot = document.createElement('p')
-  timeSlot.textContent = workHours[i];
-  smallCard.appendChild(timeSlot)
-
-  var largeCard = document.createElement('div')
-  largeCard.className = 'col-9 card';
-  row.appendChild(largeCard)
-
-  var button = document.createElement('div')
-  button.className = 'col-1 card bg-info'
-  row.appendChild(button)
-
-  var saveImage = document.createElement('img')
-  saveImage.src = '././floppy-disk.png'
-  saveImage.className = 'color-1'
-  button.appendChild(saveImage)
-
-
-}
   
-}
+  for (let i = 0; i < workHours.length; i++) {
+    var row = document.createElement('div');
+    row.className = 'row'
+    container.appendChild(row)
 
+    var smallCard = document.createElement('div')
+    smallCard.className = 'col-2 card box1';
+    // smallCard.textContent = workHours[i];
+    row.appendChild(smallCard)
+
+    var timeSlot = document.createElement('p')
+    timeSlot.textContent = workHours[i];
+    smallCard.appendChild(timeSlot)
+
+    var largeCard = document.createElement('div')
+    largeCard.className = 'col-9 card box2';
+    row.appendChild(largeCard)
+
+    var button = document.createElement('div')
+    button.className = 'col-1 card bg-info'
+    row.appendChild(button)
+
+    var saveImage = document.createElement('img')
+    saveImage.src = '././floppy-disk.png'
+    saveImage.className = 'color-1'
+    button.appendChild(saveImage)
+    oops(i);
+  }
+
+};
 buildTimes();
+
+function oops(time) {
+// for (let i = 0; i < workHours.length; i++) {
+console.log(workHours[time])
+if(timeNow > workHours[time])  {
+  document.querySelector('div.box1').classList.add('bg-secondary')
+  document.querySelector('div.box2').classList.add('bg-secondary')
+} else if(timeNow <= workHours[time]) {
+  document.querySelector('div.box1').classList.add('bg-primary')
+  document.querySelector('div.box2').classList.add('bg-primary')
+}
+}
+// }
+
+
+
+
+// compare times to determine if time has passed.
+// for (let i = 0; i < workHours.length; i++) {
+//   if(timeNow > workHours[i]){
+//     document.querySelector('.row').classList.add('bg-secondary')
+//   } else if (timeNow < workHours[i]) {
+//     console.log(`oops, this is wrong`)
+//   }
+  
+//}
